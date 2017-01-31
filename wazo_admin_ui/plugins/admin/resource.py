75dbefc4684752af16216ad7f2651e66b406cdcb
Francois Blackburn <blackburnfrancois@gmail.com>
2017-01-31 14:04:25 -0500

    admin: use flask_login instead of flask_security

--- wazo_admin_ui/plugins/admin/resource.py
+++ wazo_admin_ui/plugins/admin/resource.py
@@ -7,15 +7,15 @@ import logging
 from flask import render_template
 
 from flask_classful import FlaskView
+from flask_login import login_required
 from flask_menu.classy import classy_menu_item
-from flask_security.decorators import login_required
 
 logger = logging.getLogger(__name__)
 
 
 class Admin(FlaskView):
-    decorators = [login_required]
 
     @classy_menu_item('.dashboard', 'Dashboard', order=0, icon="dashboard")
+    @login_required
     def get(self):
         return render_template('admin.html')
