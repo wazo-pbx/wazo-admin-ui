f8f9b67fa631083ada928145cf289c1daadeb29e
Francois Blackburn <blackburnfrancois@gmail.com>
2017-03-02 16:14:28 -0500

    service: add BaseConfdExtensionService

--- wazo_admin_ui/helpers/service.py
+++ wazo_admin_ui/helpers/service.py
@@ -2,8 +2,12 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+import logging
+
 from flask_login import current_user
 from xivo_confd_client import Client as ConfdClient
+
+logger = logging.getLogger(__name__)
 
 
 class BaseConfdService(object):
@@ -27,21 +31,101 @@ class BaseConfdService(object):
         return {self.resource: resource_client.get(resource_id)}
 
     def update(self, resources):
-        resource_client = getattr(self._confd, self.confd_resource)
         resource = resources.get(self.resource)
         if not resource:
             return
 
+        resource_client = getattr(self._confd, self.confd_resource)
         resource_client.update(resource)
 
     def create(self, resources):
-        resource_client = getattr(self._confd, self.confd_resource)
         resource = resources.get(self.resource)
         if not resource:
             return
 
+        resource_client = getattr(self._confd, self.confd_resource)
         resource_client.create(resource)
 
     def delete(self, resource_id):
         resource_client = getattr(self._confd, self.confd_resource)
         resource_client.delete(resource_id)
+
+
+class BaseConfdExtensionService(BaseConfdService):
+
+    def _extract_resource_extension(self, resources):
+        resource = resources.get(self.resource)
+        extension = resources.get('extension')
+        if not resource or not extension:
+            logger.debug('Missing %s or extension resource', self.confd_resource)
+            return
+        return resource, extension
+
+    def create(self, resources):
+        super(BaseConfdExtensionService, self).create(resources)
+        resource, extension = self._extract_resource_extension(resources)
+        self.create_extension(extension, resource)
+
+    def update(self, resources):
+        super(BaseConfdExtensionService, self).update(resources)
+        resource, extension = self._extract_resource_extension(resources)
+        self.update_extension(extension, resource)
+
+    def delete(self, resource_id):
+        self.delete_extension(resource_id)
+        super(BaseConfdExtensionService, self).delete(resource_id)
+
+    def create_extension(self, extension, resource):
+        if resource and extension:
+            self._add_extension(extension, resource)
+
+    def update_extension(self, extension, resource):
+        if not extension or not resource:
+            return
+
+        existing_extension = self._get_main_extension(resource)
+
+        if extension.get('exten') and existing_extension:
+            self._update_extension(extension, existing_extension)
+
+        elif extension.get('exten'):
+            self._add_extension(extension, resource)
+
+        elif not extension.get('exten') and existing_extension:
+            self._remove_extension(existing_extension, resource)
+
+    def delete_extension(self, resource_id):
+        resource_client = getattr(self._confd, self.confd_resource)
+        resource = resource_client.get(resource_id)
+        for extension in resource['extensions']:
+            self._remove_extension(extension, resource)
+
+    def _add_extension(self, extension, resource):
+        extension = self._confd.extensions.create(extension)
+        if extension:
+            resource_client = getattr(self._confd, self.confd_resource)
+            resource_client(resource).add_extension(extension)
+
+    def _update_extension(self, extension, existing_extension):
+        if existing_extension.get('exten') == extension.get('exten') and \
+           existing_extension.get('context') == extension.get('context'):
+            return
+
+        extension['id'] = existing_extension['id']
+        self._confd.extensions.update(extension)
+
+    def _remove_extension(self, extension, resource):
+        resource_client = getattr(self._confd, self.confd_resource)
+        resource_client(resource).remove_extension(extension)
+        self._confd.extensions.delete(extension)
+
+    def _get_main_extension(self, resource):
+        resource_id = resource.get('uuid', resource.get('id'))
+        if not resource_id:
+            logger.debug('Unable to extract resource_id from %s resource', self.confd_resource)
+            return
+
+        resource_client = getattr(self._confd, self.confd_resource)
+        for extension in resource_client.get(resource_id)['extensions']:
+            return extension
+        return None
--- wazo_admin_ui/helpers/tests/test_service.py
+++ wazo_admin_ui/helpers/tests/test_service.py
@@ -7,7 +7,7 @@ import unittest
 from hamcrest import assert_that, equal_to
 from mock import Mock
 
-from ..service import BaseConfdService
+from ..service import BaseConfdService, BaseConfdExtensionService
 
 
 class TestBaseConfdService(unittest.TestCase):
@@ -53,3 +53,149 @@ class TestBaseConfdService(unittest.TestCase):
     def test_delete(self):
         self.service.delete(42)
         self._resource1.delete.assert_called_once_with(42)
+
+
+class TestBaseConfdExtensionService(unittest.TestCase):
+
+    def setUp(self):
+        self._resource1 = Mock()
+        self._extensions = Mock()
+        BaseConfdService.confd_resource = 'resource1'
+        BaseConfdService.resource = 'resource1'
+        BaseConfdService._confd = Mock(resource1=self._resource1,
+                                       extensions=self._extensions)
+        confd_config = {}
+        self.service = BaseConfdExtensionService(confd_config)
+
+    def _assert_no_confd_call(self):
+        self._extensions.create.assert_not_called()
+        self._extensions.update.assert_not_called()
+        self._extensions.delete.assert_not_called()
+        self._resource1.assert_not_called()
+        self._resource1.return_value.remove_extension.assert_not_called()
+        self._resource1.return_value.add_extension.assert_not_called()
+
+    def test_update_extension_when_no_extension(self):
+        resource = {'id': 3}
+        extension = None
+        self.service.update_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_update_extension_when_no_resource(self):
+        resource = {'id': 3}
+        extension = None
+        self.service.update_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_update_extension_when_extension_is_empty(self):
+        resource = {'id': 3}
+        extension = {}
+        self.service.update_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_update_extension_when_exten_is_removed(self):
+        existing_extension = {'exten': None, 'context': 'default', 'id': 42}
+        self._resource1.get.return_value = {'extensions': [existing_extension]}
+        resource = {'id': 3}
+        extension = {'exten': None, 'context': 'default'}
+        self.service.update_extension(extension, resource)
+
+        self._extensions.delete.assert_called_once_with(existing_extension)
+        self._resource1.assert_called_once_with(resource)
+        self._resource1.return_value.remove_extension.assert_called_once_with(existing_extension)
+
+    def test_update_extension_when_no_exten_key(self):
+        existing_extension = {'exten': None, 'context': 'default', 'id': 42}
+        self._resource1.get.return_value = {'extensions': [existing_extension]}
+        resource = {'id': 3}
+        extension = {'context': 'default'}
+        self.service.update_extension(extension, resource)
+
+        self._extensions.delete.assert_called_once_with(existing_extension)
+        self._resource1.assert_called_once_with(resource)
+        self._resource1.return_value.remove_extension.assert_called_once_with(existing_extension)
+
+    def test_update_extension_when_no_exten_and_no_existing_extension(self):
+        self._resource1.get.return_value = {'extensions': []}
+        resource = {'id': 3}
+        extension = {'exten': None, 'context': 'default'}
+        self.service.update_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_update_extension_when_same_extension_and_existing_extension(self):
+        exten, context = '123', 'default'
+        existing_extension = {'exten': exten, 'context': context, 'id': 42}
+        self._resource1.get.return_value = {'extensions': [existing_extension]}
+        resource = {'id': 3}
+        extension = {'exten': exten, 'context': context}
+        self.service.update_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_update_extension_when_different_extension_and_existing_extension(self):
+        existing_extension = {'exten': '123', 'context': 'default', 'id': 42}
+        self._resource1.get.return_value = {'extensions': [existing_extension]}
+        resource = {'id': 3}
+        extension = {'exten': '456', 'context': 'default'}
+        self.service.update_extension(extension, resource)
+
+        expected_call = extension
+        expected_call['id'] = 42
+        self._extensions.update.assert_called_once_with(expected_call)
+
+    def test_update_extension_when_extension_and_no_existing_extension(self):
+        self._resource1.get.return_value = {'extensions': []}
+        resource = {'id': 3}
+        extension = {'exten': '456', 'context': 'default'}
+        self._extensions.create.return_value = extension
+
+        self.service.update_extension(extension, resource)
+
+        self._extensions.create.assert_called_once_with(extension)
+        self._resource1.assert_called_once_with(resource)
+        self._resource1.return_value.add_extension.assert_called_once_with(extension)
+
+    def test_create_extension_when_no_extension(self):
+        resource = {'id': 3}
+        extension = None
+        self.service.create_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_create_extension_when_no_resource(self):
+        resource = {'id': 3}
+        extension = None
+        self.service.create_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_create_extension_when_extension_is_empty(self):
+        resource = {'id': 3}
+        extension = {}
+        self.service.create_extension(extension, resource)
+        self._assert_no_confd_call()
+
+    def test_create_extension_when_extension_and_resource(self):
+        resource = {'id': 3}
+        extension = {'exten': '1234', 'context': 'default'}
+        self._extensions.create.return_value = extension
+
+        self.service.create_extension(extension, resource)
+
+        self._extensions.create.assert_called_once_with(extension)
+        self._resource1.assert_called_once_with(resource)
+        self._resource1.return_value.add_extension.assert_called_once_with(extension)
+
+    def test_delete_extension_when_no_extension(self):
+        resource = {'id': 42, 'extensions': []}
+        self._resource1.get.return_value = resource
+        self.service.delete_extension(resource['id'])
+        self._assert_no_confd_call()
+
+    def test_delete_extension_when_extension_and_resource(self):
+        extension = {'id': 1, 'exten': '1234', 'context': 'default'}
+        resource = {'id': 42, 'extensions': [extension]}
+        self._resource1.get.return_value = resource
+
+        self.service.delete_extension(resource['id'])
+
+        self._extensions.delete.assert_called_once_with(extension)
+        self._resource1.assert_called_once_with(resource)
+        self._resource1.return_value.remove_extension.assert_called_once_with(extension)
