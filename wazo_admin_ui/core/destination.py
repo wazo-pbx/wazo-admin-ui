0de86d99489ad30db3b4a8cbef144f03e537d8ec
Francois Blackburn <blackburnfrancois@gmail.com>
2017-03-15 14:47:29 -0400

    add destination blueprint
    reason: All plugins can register a view to this blueprint. So we need to
    call register_blueprint after load_plugins

---
+++ wazo_admin_ui/core/destination.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from wazo_admin_ui.helpers.plugin import create_blueprint
+
+destination_blueprint = create_blueprint('destination', __name__)
