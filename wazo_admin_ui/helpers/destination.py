33f01958d8132b83a570f3a462e98fbf26a08559
Francois Blackburn <fblackburn@wazo.community>
2017-04-10 15:37:12 -0400

    add new helpers in destination to remove marshmallow

--- wazo_admin_ui/helpers/destination.py
+++ wazo_admin_ui/helpers/destination.py
@@ -2,12 +2,12 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from flask_wtf import FlaskForm
 from marshmallow import fields, post_load, post_dump
 from wtforms.fields import SelectField, FormField, HiddenField
 from wtforms.utils import unset_value
 
 from wazo_admin_ui.helpers.mallow import BaseSchema
+from .form import BaseForm
 
 
 _destination_choices = set()
@@ -23,7 +23,7 @@ def register_destination_form(type_id, type_label, form):
     setattr(DestinationForm, type_id, FormField(form))
 
 
-class DestinationForm(FlaskForm):
+class DestinationForm(BaseForm):
 
     type = SelectField('Destination', choices=[])
 
@@ -31,6 +31,33 @@ class DestinationForm(FlaskForm):
         super(DestinationForm, self).__init__(*args, **kwargs)
         self.type.choices = [('none', 'None')] + list(_destination_choices)
         self.listing_urls = listing_urls
+
+    def to_dict(self):
+        data = super(DestinationForm, self).to_dict()
+
+        destination_type = data.get('type')
+        if not destination_type:
+            return {}
+
+        destination = data.get(destination_type, {})
+        result = self._convert_all_empty_string_to_none(destination)
+        result['type'] = destination_type
+        return result
+
+    def _convert_all_empty_string_to_none(self, data):
+        result = {}
+        for key, val in data.iteritems():
+            result[key] = val if val != '' else None
+        return result
+
+    def process(self, formdata=None, obj=None, data=None, **kwargs):
+        destination = kwargs
+        if destination and 'type' in destination:
+            destination_type = destination.pop('type')
+            kwargs = {'type': destination_type,
+                      destination_type: kwargs}
+
+        super(DestinationForm, self).process(formdata, obj, data, **kwargs)
 
 
 class DestinationField(FormField):
@@ -45,11 +72,19 @@ class DestinationField(FormField):
             self.form.type.label.text = self.destination_label
 
 
-class FallbacksForm(FlaskForm):
+class FallbacksForm(BaseForm):
     busy_destination = DestinationField('Busy')
     congestion_destination = DestinationField('Congestion')
     fail_destination = DestinationField('Fail')
     noanswer_destination = DestinationField('No answer')
+
+    def to_dict(self):
+        data = super(FallbacksForm, self).to_dict()
+
+        for key, val in data.iteritems():
+            if val.get('type') == 'none':
+                data[key] = None
+        return data
 
 
 class DestinationHiddenField(HiddenField):
--- wazo_admin_ui/helpers/tests/test_destination.py
+++ wazo_admin_ui/helpers/tests/test_destination.py
@@ -4,9 +4,116 @@
 
 import unittest
 
-from hamcrest import assert_that, equal_to, empty
+from flask import Flask
+from hamcrest import assert_that, equal_to, empty, has_entries
+from wtforms import StringField, FormField, IntegerField
 
-from ..destination import DestinationSchema
+from ..destination import DestinationSchema, DestinationForm, FallbacksForm
+from ..form import BaseForm
+
+
+app = Flask('test_wazo_admin_ui')
+
+
+class TestDestinationForm(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+
+        class UserForm(BaseForm):
+            user_id = IntegerField()
+            timeout = StringField()
+
+        DestinationForm.user = FormField(UserForm)
+
+    def test_to_dict(self):
+        data = {'type': 'user',
+                'user-user_id': 1,
+                'user-timeout': '2'}
+
+        with app.test_request_context(method='POST', data=data):
+            form = DestinationForm()
+        result = form.to_dict()
+
+        assert_that(result, equal_to({'type': 'user',
+                                      'user_id': 1,
+                                      'timeout': '2'}))
+
+    def test_to_dict_with_no_type(self):
+        data = {}
+
+        with app.test_request_context(method='POST', data=data):
+            form = DestinationForm()
+        result = form.to_dict()
+
+        assert_that(result, empty())
+
+    def test_to_dict_with_no_destination_values(self):
+        data = {'type': 'none'}
+
+        with app.test_request_context(method='POST', data=data):
+            form = DestinationForm()
+        result = form.to_dict()
+
+        assert_that(result, equal_to({'type': 'none'}))
+
+    def test_to_dict_with_empty_string(self):
+        data = {'type': 'user',
+                'user-user_id': 1,
+                'user-timeout': ''}
+
+        with app.test_request_context(method='POST', data=data):
+            form = DestinationForm()
+        result = form.to_dict()
+
+        assert_that(result, equal_to({'type': 'user',
+                                      'user_id': 1,
+                                      'timeout': None}))
+
+    def test_process(self):
+        data = {'type': 'user',
+                'user-user_id': 1,
+                'user-timeout': '2'}
+
+        with app.test_request_context(method='POST', data=data):
+            form = DestinationForm()
+
+        assert_that(form.data, has_entries(type='user',
+                                           user={'user_id': 1,
+                                                 'timeout': '2'}))
+
+    def test_process_with_no_type(self):
+        data = {}
+
+        with app.test_request_context(method='POST', data=data):
+            form = DestinationForm()
+
+        assert_that(form.data, has_entries(type='None'))
+
+    def test_process_with_none_as_destination(self):
+        data = {'type': 'none'}
+
+        with app.test_request_context(method='POST', data=data):
+            form = DestinationForm()
+
+        assert_that(form.data, has_entries(type='none'))
+
+
+class TestFallbacksForm(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+
+    def test_to_dict_with_none_as_destination(self):
+        data = {'busy_destination-type': 'none'}
+
+        with app.test_request_context(method='POST', data=data):
+            form = FallbacksForm()
+        result = form.to_dict()
+
+        assert_that(result, has_entries(busy_destination=None))
 
 
 class TestDestinationSchema(unittest.TestCase):
