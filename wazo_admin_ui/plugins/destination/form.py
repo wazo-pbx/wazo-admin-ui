d1d7db0f0ccf065af30fca1384ea2feb3076151b
Francois Blackburn <fblackburn@wazo.community>
2017-05-01 14:26:18 -0400

    destination: add HangupDestination

--- wazo_admin_ui/plugins/destination/form.py
+++ wazo_admin_ui/plugins/destination/form.py
@@ -2,10 +2,35 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from wtforms.fields import StringField
-from wtforms.validators import InputRequired, Length
+from wtforms.fields import StringField, SelectField, FormField
+from wtforms.fields.html5 import IntegerField
+from wtforms.validators import InputRequired, Length, NumberRange
 
 from wazo_admin_ui.helpers.form import BaseForm
+from wazo_admin_ui.helpers.destination import BaseDestinationForm
+
+
+class HangupCongestionDestination(BaseForm):
+
+    timeout = IntegerField('Timeout', [NumberRange(min=0)])
+
+
+class HangupBusyDestination(BaseForm):
+
+    timeout = IntegerField('Timeout', [NumberRange(min=0)])
+
+
+class HangupNormalDestination(BaseForm):
+    pass
+
+
+class HangupDestination(BaseDestinationForm):
+    select_field = 'cause'
+
+    cause = SelectField('Cause', choices=[('normal', 'Normal'), ('busy', 'Busy'), ('congestion', 'Congestion')])
+    busy = FormField(HangupBusyDestination)
+    congestion = FormField(HangupBusyDestination)
+    normal = FormField(HangupNormalDestination)
 
 
 class CustomDestination(BaseForm):
--- wazo_admin_ui/plugins/destination/plugin.py
+++ wazo_admin_ui/plugins/destination/plugin.py
@@ -4,11 +4,12 @@
 
 from wazo_admin_ui.helpers.destination import register_destination_form
 
-from .form import CustomDestination, NoneDestination
+from .form import CustomDestination, HangupDestination, NoneDestination
 
 
 class Plugin(object):
 
     def load(self, dependencies):
+        register_destination_form('hangup', 'Hangup', HangupDestination)
         register_destination_form('custom', 'Custom', CustomDestination)
         register_destination_form('none', 'None', NoneDestination)
