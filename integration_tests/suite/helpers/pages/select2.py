e016466fa5df1b5f7e6dc7833607befa649e71a8
Cedric Abunar <cabunar@wazo.io>
2017-12-04 15:04:46 -0500

    fix integration tests with new interface

--- integration_tests/suite/helpers/pages/select2.py
+++ integration_tests/suite/helpers/pages/select2.py
@@ -3,7 +3,7 @@
 
 # Based from https://gist.github.com/rafaelugolini/d2067a8c8c54026ac029
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -53,8 +53,16 @@ class Select2(object):
     def dropdown(self):
         return self.browser.find_element_by_css_selector('span.select2-dropdown')
 
+    def ajax_complete(self, driver):
+        try:
+            return 0 == driver.execute_script("return jQuery.active")
+        except WebDriverException:
+            pass
+
     @property
     def items(self):
         self.open()
+        ajax_complete = getattr(self, 'ajax_complete')
+        WebDriverWait(self.browser, self.TIMEOUT).until(ajax_complete, "Timeout waiting for page to load")
         return self.dropdown.find_elements_by_css_selector(
             'ul.select2-results__options li.select2-results__option')
