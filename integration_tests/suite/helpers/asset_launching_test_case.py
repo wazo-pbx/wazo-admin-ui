a142b044e323b5dc7ad00e550faafcc908953eda
Francois Blackburn <fblackburn@wazo.community>
2018-01-31 08:03:46 -0500

    int tests: remove unused env variables

--- integration_tests/suite/helpers/asset_launching_test_case.py
+++ integration_tests/suite/helpers/asset_launching_test_case.py
@@ -1,4 +1,4 @@
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 import os
@@ -31,11 +31,8 @@ class AdminUIAssetLaunchingTestCase(AssetLaunchingTestCase):
     @classmethod
     def setup_browser(cls):
         virtual = os.environ.get('VIRTUAL_DISPLAY', '1') == '1'
-        username = os.environ.get('WEBI_USERNAME', 'root')
-        password = os.environ.get('WEBI_PASSWORD', 'password')
-        url = os.environ.get('ADMIN_UI_URL')
-        if not url:
-            port = cls.service_port(9296, 'admin-ui')
-            url = 'https://localhost:{port}'.format(port=port)
-        Page.CONFIG['base_url'] = url
+        username = 'xivo-auth-mock-doesnt-care-about-username'
+        password = 'xivo-auth-mock-doesnt-care-about-password'
+        port = cls.service_port(9296, 'admin-ui')
+        Page.CONFIG['base_url'] = 'https://localhost:{port}'.format(port=port)
         return Browser(username, password, virtual)
